f33e57c11ec38f79be0a61a71ec887fe2f94cea5
thanos-theod <thanostheodorou13@gmail.com>
2026-04-27 15:15:05 +0300

    Initialize main layout with sections and navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
-function App() {
-  return <></>;
-}
+// Layout
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
-export default App;
+// Sections
+import Hero from "./components/sections/Hero/Hero.jsx";
+import About from "./components/sections/About/About.jsx";
+import Work from "./components/sections/Work/Work.jsx";
+import Contact from "./components/sections/Contact/Contact.jsx";
+
+export const App = () => {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <About />
+        <Work />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  );
+};
