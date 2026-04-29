b4cd1fc9d16d778ed8cbedf067d002d8d901e47a
thanos-theod <thanostheodorou13@gmail.com>
2026-04-29 17:35:42 +0300

    Add scroll effect, active section tracking and navigation links

--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -1,13 +1,49 @@
+import { useState, useEffect } from "react";
+import { useActiveSection } from "../../../hooks/useActiveSection";
+
 import { Logo } from "../../ui/Logo/Logo";
+import { NavLinks } from "./components/NavLinks";
+
+import classes from "./Navbar.module.css";
 
 export const Navbar = () => {
+  const [scrolled, setScrolled] = useState(false);
+
+  const activeSection = useActiveSection(["home", "about", "work", "contact"]);
+
+  const handleNav = (href) => {
+    const targetSection = document.querySelector(href);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
+  // scroll effect
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 40) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <>
-      <header>
-        <div>
+      <header className={classes["navbar-header"]}>
+        <div
+          className={`${classes["navbar-container"]} ${scrolled ? classes.scrolled : ""}`}
+        >
           <Logo />
-          {/* <NavLinks />
-          <NavbarActions />
+          <nav className={classes["navbar-desktop-nav"]}>
+            <NavLinks activeSection={activeSection} onNavigate={handleNav} />
+          </nav>
+          {/* <NavbarActions />
           <MobileMenuToggle /> */}
         </div>
       </header>
