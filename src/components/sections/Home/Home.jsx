f572779d84513f70da53a6c2a84ef73dde18b515
thanos-theod <thanostheodorou13@gmail.com>
2026-05-05 22:11:35 +0300

    Remove unused code

--- src/components/sections/Home/Home.jsx
+++ src/components/sections/Home/Home.jsx
@@ -5,8 +5,6 @@ import classes from "./Home.module.css";
 export const Home = () => {
   return (
     <section id="home" className={classes["home-section"]}>
-      <div className={classes["home-radial-highlight"]} />
-
       <div className={classes["home-container"]}>
         <div className={classes["home-grid"]}>
           {/* COMPONENT 1 - LEFT CONTENT */}
