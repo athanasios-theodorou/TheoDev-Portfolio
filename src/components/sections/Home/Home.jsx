53040abd1fab7c6f1bbf18f1843b8a4c845c886c
thanos-theod <thanostheodorou13@gmail.com>
2026-05-06 22:17:14 +0300

    feat(home): add delayed animation trigger to control home entrance timing

--- src/components/sections/Home/Home.jsx
+++ src/components/sections/Home/Home.jsx
@@ -1,14 +1,29 @@
+import { useState, useEffect } from "react";
+
 import { HomeContent } from "./subcomponents/HomeContent";
 
 import classes from "./Home.module.css";
 
+const ANIMATION_DELAY = 600;
+
 export const Home = () => {
+  const [shouldAnimate, setShouldAnimate] = useState(false);
+
+  // Small delay to ensure background/grid is visible first
+  useEffect(() => {
+    const animationDelayTimer = setTimeout(() => {
+      setShouldAnimate(true);
+    }, ANIMATION_DELAY);
+
+    return () => clearTimeout(animationDelayTimer);
+  }, []);
+
   return (
     <section id="home" className={classes["home-section"]}>
       <div className={classes["home-container"]}>
         <div className={classes["home-grid"]}>
           {/* COMPONENT 1 - LEFT CONTENT */}
-          <HomeContent />
+          <HomeContent shouldAnimate={shouldAnimate} />
           {/* COMPONENT 2 - RIGHT CONTENT */}
           <div></div>
         </div>
