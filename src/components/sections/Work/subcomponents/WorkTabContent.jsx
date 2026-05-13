d5523ab72891d5220105ae612b59ac7ddbcbe3cb
thanos-theod <thanostheodorou13@gmail.com>
2026-05-13 14:21:59 +0300

    feat(work-content): connect project data flow through WorkTabContent component

--- src/components/sections/Work/subcomponents/WorkTabContent.jsx
+++ src/components/sections/Work/subcomponents/WorkTabContent.jsx
@@ -1,12 +1,27 @@
-import { certificates, techStack } from "../../../../assets/data/portfolio";
+import {
+  projects,
+  certificates,
+  techStack,
+} from "../../../../assets/data/portfolio";
+import { ProjectCard } from "./ProjectCard";
 import { CertCard } from "./CertCard";
 import { TechCard } from "./TechCard";
 
 import classes from "./WorkTabContent.module.css";
 
-export const WorkTabContent = () => {
+export const WorkTabContent = ({ onOpenProject }) => {
   return (
     <div>
+      <div key="projects" className={classes["work-projects-grid"]}>
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.id}
+            project={project}
+            onOpen={onOpenProject}
+          />
+        ))}
+      </div>
+
       <div key="certificates" className={classes["work-certs-grid"]}>
         {certificates.map((cert, i) => (
           <CertCard key={cert.title} cert={cert} index={i} />
